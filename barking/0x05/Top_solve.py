1570cb46f8d69cdd9cfb206f6dd1ba3474949f0c
dodo <do151015@gmail.com>
2022-03-10 22:37:48 +0900

    dd

---
+++ barking/0x05/Top_solve.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(sys.stdin.readline())
+top = list(map(int, sys.stdin.readline().split()))
+result = []
+stack = []
+
+for i in range(len(top)):
+    while stack:
+        if top[i] > stack[-1][1]:
+            stack.pop()
+        else:
+            result.append(stack[-1][0] + 1)
+            break
+    if not stack:
+        result.append(0)
+    stack.append((i, top[i]))
+
+print(" ".join(map(str, result)))
