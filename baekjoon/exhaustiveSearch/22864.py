96ac3f696fa027049b75d11c232f1e23f8382137
dodo <do151015@gmail.com>
2022-12-02 23:23:21 +0900

    1201

---
+++ baekjoon/exhaustiveSearch/22864.py
@@ -0,0 +1,14 @@
+A, B, C, M = map(int, input().split())
+
+tired = 0
+work = 0
+for time in range(1, 25):
+    if tired <= M - A:
+        tired += A
+        work += B
+    else: # 번아웃
+        tired -= C
+        if tired < 0:
+            tired = 0
+
+print(work)
