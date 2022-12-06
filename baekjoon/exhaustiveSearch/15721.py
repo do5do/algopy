56237f422cdd65889c9a304688dfee05ccbfae80
dodo <do151015@gmail.com>
2022-12-07 00:09:08 +0900

    1207

--- baekjoon/exhaustiveSearch/15721.py
+++ baekjoon/exhaustiveSearch/15721.py
@@ -1,34 +1,45 @@
-A, T, inputs = [int(input().strip()) for _ in range(3)]
+A, T, answer = [int(input().strip()) for _ in range(3)]
 
-repeat, round = 1, 1
+repeat, round = 0, 1
 # 뻔 개수, 데기 개수
-fcnt, dcnt = 1, 0
+fcnt, dcnt = 0, 0
 cnt, talk = 0, 0
 
 flag = False
 result = 0
 
 while cnt < T:
-    for i in range(1, A):
-        repeat += 1
-        if repeat <= 4: # 뻔 데기 뻔 데기
-            if talk == 0:
-                talk = 1
-                dcnt += 1
-            else:
-                talk = 0
+    for i in range(A):
+        if repeat <= 3: # 뻔 데기 뻔 데기
+            if talk == 0: # 뻔
                 fcnt += 1
+                talk = 1
+            else: # 데기
+                dcnt += 1
+                talk = 0
         else:
             for _ in range(round + 1):
-                if talk == 0:
-                    talk = 0
+                if talk == 0: # 뻔
                     fcnt += 1
-                else:
-                    talk = 1
+                    talk = 0
+                else: # 데기
                     dcnt += 1
-                i += 1
+                    talk = 1
 
-        if inputs == 0:
+                if i < A:
+                    i += 1
+                else:
+                    i = 0
+
+            if talk == 0: # 데기를 count하도록 바꿔줌
+                talk = 1
+            else:
+                round += 1
+                repeat = -1
+
+        repeat += 1
+
+        if answer == 0:
             cnt = fcnt
         else:
             cnt = dcnt
@@ -37,8 +48,6 @@ while cnt < T:
             result = i
             flag = True
             break
-
-    round += 1
 
     if flag:
         break
