af0f7e820823a143b3abc837f88a9d86f50c4c3d
do5do <do151015@gmail.com>
2023-02-05 16:49:45 +0900

    0205

--- programmers/hash/Clothes2.py
+++ programmers/hash/Clothes2.py
@@ -1,3 +1,4 @@
+# 다시 풀기 : 2회차
 def solution(clothes):
     answer = 1
     clothesDict = {}
@@ -21,4 +22,5 @@ print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["gr
     - headgear 2, eyewear1일때 각각 착용하지 않는 경우가 있다.
     - 착용하지 않는 경우까지 하면 headgear3, eyewear2가지이고 총 경우의 수는 3 x 2이다.
     - 여기서 모두 착용하지 않는 경우 1가지를 빼준다.
+=> 풀이가 사실 생각안나긴 했다. 비슷한 문제를 다시 풀어봐야겠다. 30분 소요
 '''
