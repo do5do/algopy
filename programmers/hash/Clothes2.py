7be67924bebf9325dbe8aa63b68b2e49aa1d44f3
do5do <do151015@gmail.com>
2023-02-05 16:46:31 +0900

    0202

---
+++ programmers/hash/Clothes2.py
@@ -0,0 +1,24 @@
+def solution(clothes):
+    answer = 1
+    clothesDict = {}
+
+    for cloth in clothes:
+        clothesDict.setdefault(cloth[1], 1)  # 착용하지 않는 경우를 포함하여 default value를 1로 설정
+        clothesDict[cloth[1]] += 1
+
+    for val in clothesDict.values(): # 곱의 경우의 수 구함
+        answer *= val
+
+    return answer - 1 # 모두 착용하지 않는 경우 제외
+
+print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
+
+
+'''
+위장
+문제 풀이
+- 종류별로 경우의 수를 구한다.
+    - headgear 2, eyewear1일때 각각 착용하지 않는 경우가 있다.
+    - 착용하지 않는 경우까지 하면 headgear3, eyewear2가지이고 총 경우의 수는 3 x 2이다.
+    - 여기서 모두 착용하지 않는 경우 1가지를 빼준다.
+'''
--- programmers/hash/ClothesRe.py
+++ programmers/hash/ClothesRe.py
@@ -18,9 +18,10 @@ print(answer - 1)
 '''
 문제 풀이
 - 각각 한 종류씩 입을 수도 있고 안 입을 수도 있다. 하지만 모두 안 입는 경우는 없다.
-- 즉, headgear를 입는 경우와 안 입는 경우가 있고 eyewear를 입는 경우와 안입는 경우가 있다.
-    - headgear(2), eyewear(1)개일 때 headgear1을 입는 경우, headgear2를 입는 경우, headgear를 모두 안 입는 경우로 총 headgear는 3가지다.
-    - eyewear1을 입는 경우, eyewear를 안입는 경우 = 2가지
-    - headgear와 eyewear를 고르는 경우는 동시에 발생한다. => 곱의 법칙으로 3 * 2로 총 6가지 경우인데, 여기엔 모두 안입는 경우가 포함되어 있다.
+- 즉, headgear를 입는 경우와 안 입는 경우가 있고 eyewear를 입는 경우와 안 입는 경우가 있다.
+    - headgear(2), eyewear(1)개일 때 headgear1을 입는 경우, headgear2를 입는 경우, headgear를 안 입는 경우로 총 headgear는 3가지다.
+    - eyewear1을 입는 경우, eyewear를 안 입는 경우 = 2가지
+    - headgear와 eyewear를 고르는 경우는 동시에 발생한다. => 곱의 법칙으로 3 * 2로 총 6가지 경우인데,
+    여기엔 모두 안입는 경우(headgear 안 입는 경우, eyewear 안 입는 경우)가 포함되어 있다.
     - 하지만 모두 안입는 경우는 없으니 답은 6가지 중 모두 안입는 경우 1을 빼서 5가 된다.
 '''
