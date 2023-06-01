710a839d20434143f6935087ccc75f370ed7d09b
do5do <do151015@gmail.com>
2023-06-01 23:35:51 +0900

    0601

--- programmers/exhautiveSearch/TwoElectronicRe.py
+++ programmers/exhautiveSearch/TwoElectronicRe.py
@@ -30,7 +30,8 @@ def solution(n, wires):
     return answer
 
 
-print(solution(4, [[1,2],[2,3],[3,4]]))
+# print(solution(4, [[1,2],[2,3],[3,4]]))
+print(solution(9, [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))
 
 
 '''
